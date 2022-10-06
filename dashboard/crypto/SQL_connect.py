ab7dc5ced7c82f43faab9a75b9a572a166d5e5bf
james fryer <jamesfryer1998@outlook.com>
2022-10-06 13:20:55 +0100

    first commit

---
+++ dashboard/crypto/SQL_connect.py
@@ -0,0 +1,60 @@
+import psycopg2
+
+class SQLConnect:
+    def __init__(self, ticker, host, user, password):
+        self.connect_string = f'host={host} dbname=postgres user={user} password={password}'
+        self.ticker = ticker
+        self.conn = psycopg2.connect(self.connect_string)
+
+    def create_table(self):
+        query = '''
+        CREATE TABLE {ticker} (
+            id SERIAL PRIMARY KEY,
+            date DATE,
+            open FLOAT,
+            high FLOAT,
+            low FLOAT,
+            close FLOAT,
+            volume FLOAT
+        );
+        '''
+        cur = self.conn.cursor()
+
+        try:
+            cur.execute(query)
+            self.conn.commit()
+        except psycopg2.errors.DuplicateTable as error:
+            print(f'SKIP: Duplicated {self.ticker} TABLE')
+
+        cur.close()
+        self.conn.close()
+
+
+
+# connect_string = f'host=localhost dbname=postgres user=postgres password=mysecretpassword'
+# conn = psycopg2.connect(connect_string)
+# cur = conn.cursor()
+
+# ticker = 'ETH'
+# sql = f'''
+# CREATE TABLE {ticker} (
+#     id SERIAL PRIMARY KEY,
+#     date DATE,
+#     open FLOAT,
+#     high FLOAT,
+#     low FLOAT,
+#     close FLOAT,
+#     volume FLOAT
+# );
+# '''
+
+# try:
+#     cur.execute(sql)
+#     conn.commit()
+# except psycopg2.errors.DuplicateTable as error:
+#     print(f'SKIP: Duplicated {ticker} TABLE')
+
+# # Close communications with database
+# cur.close()
+# conn.close()
+
---
+++ dashboard/crypto/yfinance_download.py
@@ -0,0 +1,49 @@
+from turtle import down
+import numpy as np
+import pandas as pd
+import datetime
+import yfinance as yf
+import os
+
+class downloadData:
+    def __init__(self, file_path=None):
+        self.time = datetime.datetime.now()
+        self.file_path = file_path
+        self.file = None
+    
+    def download_save_data(self, ticker, period, recent_first=False):
+        date = self.time.date()
+        file = f'{ticker}-{date}'
+        print('Initializing search...')
+
+        if os.path.isfile(f'{self.file_path}/{file}.csv') == True:
+            print('File exists')
+        else:
+            print(f'Downloading {ticker}-USD - {period}')
+            ticker_data = yf.Ticker(f'{ticker}-USD')
+            historical_data = ticker_data.history(period=period)
+            df = pd.DataFrame(historical_data)
+            df.drop(['Dividends', 'Stock Splits'], axis=1, inplace=True)
+            df.sort_index(ascending=recent_first, inplace=True)
+            df.reset_index(inplace=True)
+            df['Date'] = pd.to_datetime(df['Date'])
+            df.dropna()
+
+            if len(df) == 0:
+                raise Exception('No data found')
+
+            if self.file_path == None:
+                raise Exception('Please specify file path.')
+        
+            df.to_csv(f'{self.file_path}/{file}.csv', sep='\t', encoding='utf-8')
+
+        self.file = file
+
+    def load_data(self):
+        data = pd.read_csv(f'{self.file_path}/{self.file}.csv', sep='\t')
+        return data
+
+download = downloadData('Cache')
+download.download_save_data('ETH', 'ytd')
+data = download.load_data()
+print(data)
