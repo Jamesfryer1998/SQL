1befbe42dd9c4f498e200d836e3d65123877d4a0
james fryer <jamesfryer1998@outlook.com>
2022-10-30 21:02:03 +0000

    price prediction update

---
+++ dashboard/crypto/price_prediction.py
@@ -0,0 +1,10 @@
+from functions import *
+
+class PricePrediction:
+    def __init__(self, ticker):
+        self.ticker = ticker
+
+    def predict(self):
+        return
+
+print(load_file('ADA'))
