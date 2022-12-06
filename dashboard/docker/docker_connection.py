ec473c1b6dcaf6cbdbb7a74757bb8792717ec7c6
james fryer <jamesfryer1998@outlook.com>
2022-12-06 10:58:57 +0000

    Please enter the commit message for your changes. Lines starting

--- dashboard/docker/docker_connection.py
+++ dashboard/docker/docker_connection.py
@@ -1,32 +1,3 @@
-# import os
-# import re
-
-# def search(name, test):
-#     search = re.search(name, test)
-#     if search != None:
-#         return 1
-#     else:
-#         return 0
-
-# def docker():
-#     test = os.popen('docker ps').read()
-#     found = []
-#     names = ['grafana', 'some-postgres']
-
-#     for name in names:
-#         found.append(search(name, test))
-
-#     if sum(found) == 2:
-#         print('Stopping docker containers...')
-#         os.system('docker stop some-postgres grafana')
-#         print('Containers stopped.')
-#     else:
-#         print('Starting docker containers...')
-#         os.system('docker start some-postgres grafana')
-#         print('Containers started.')
-        
-# docker()
-
 import os
 import re
 
